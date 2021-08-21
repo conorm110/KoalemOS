e5ad1a4ef84323a6689c05d2d744a43e58ec81e5
conorm <mjoconnor110@gmail.com>
2021-08-20 19:45:07 -0500

    #oops

---
+++ sys/fs/flp.h
@@ -0,0 +1,39 @@
+// standard base address of the primary floppy controller
+static const int floppy_base = 0x03f0;
+// standard IRQ number for floppy controllers
+static const int floppy_irq = 6;
+
+// The registers of interest. There are more, but we only use these here.
+enum floppy_registers {
+   FLOPPY_DOR  = 2,  // digital output register
+   FLOPPY_MSR  = 4,  // master status register, read only
+   FLOPPY_FIFO = 5,  // data FIFO, in DMA operation for commands
+   FLOPPY_CCR  = 7   // configuration control register, write only
+};
+
+// The commands of interest. There are more, but we only use these here.
+enum floppy_commands {
+   CMD_SPECIFY = 3,            // SPECIFY
+   CMD_WRITE_DATA = 5,         // WRITE DATA
+   CMD_READ_DATA = 6,          // READ DATA
+   CMD_RECALIBRATE = 7,        // RECALIBRATE
+   CMD_SENSE_INTERRUPT = 8,    // SENSE INTERRUPT
+   CMD_SEEK = 15,              // SEEK
+};
+
+static const char * drive_types[9] = {
+    "none",
+    "none",
+    "360kB 5.25\"",
+    "1.2MB 5.25\"",
+    "720kB 3.5\"",
+
+    "1.44MB 3.5\"",
+    "2.88MB 3.5\"",
+    "unknown type",
+    "unknown type"
+};
+
+void flp_detect();
+void floppy_write_cmd(int base, char cmd);
+unsigned char floppy_read_data(int base);
