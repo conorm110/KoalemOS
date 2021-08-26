522ccec3d702b4cde9337bca761317a49389adc6
conorm <mjoconnor110@gmail.com>
2021-08-26 18:43:52 -0500

    python formatting

--- emulation/run.py
+++ emulation/run.py
@@ -4,11 +4,13 @@ import platform
 import os.path
 from os import path
 
+
 def clean():
     if path.exists("$/"):
         shutil.rmtree("$/")
         return True
     return False
+
 
 def fs_setup():
     os.mkdir("$/")
@@ -19,11 +21,14 @@ def fs_setup():
     shutil.move("../build/build/kernel.elf", "$/kernel/kernel.elf")
     return
 
+
 def flp_setup(cmd):
-    if (input("Floppy Drive y/[n]: ") == "y"): 
-        os.system("qemu-img create $/devices/flp.img " + input("FLP Storage: "))
+    if (input("Floppy Drive y/[n]: ") == "y"):
+        os.system("qemu-img create $/devices/flp.img " +
+                  input("FLP Storage: "))
         cmd += "-drive file=$/devices/flp.img,index=1,if=floppy,format=raw"
     return cmd
+
 
 def build_kernel():
     if (platform.system() == 'Linux'):
@@ -35,18 +40,20 @@ def build_kernel():
     else:
         return False
 
+
 def move_lib():
     os.system("cp ../lib/* $/lib/")
     return
 
-def main():
-    cmd = "qemu-system-x86_64 -kernel $/kernel/kernel.elf " # QEMU Base Start Command
 
-    if clean(): 
+def main():
+    cmd = "qemu-system-x86_64 -kernel $/kernel/kernel.elf "  # QEMU Base Start Command
+
+    if clean():
         if build_kernel():
-            fs_setup() # Setup FileSystem
-            cmd = flp_setup(cmd) # Setup FLP Drive
-            os.system(cmd) # Start VM
+            fs_setup()  # Setup FileSystem
+            cmd = flp_setup(cmd)  # Setup FLP Drive
+            os.system(cmd)  # Start VM
         else:
             print("Error: Kernel Could Not Be Created")
             exit()
