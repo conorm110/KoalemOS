24491ff581345da751716863fc834a6f0c6a1fd8
conorm <mjoconnor110@gmail.com>
2021-08-26 14:18:22 -0500

    update formatting

--- os/helpers/os_functions.asm
+++ os/helpers/os_functions.asm
@@ -3,6 +3,7 @@
 ; This source code is licensed under the BSD-style license found in the
 ; LICENSE file in the root directory of this source tree. 
 
+; Linking
 global halt_program
 
 ; halt_program() - hlt
--- sys/kernel/io/interrupts/interrupts.asm
+++ sys/kernel/io/interrupts/interrupts.asm
@@ -3,6 +3,7 @@
 ; This source code is licensed under the BSD-style license found in the
 ; LICENSE file in the root directory of this source tree. 
 
+; Linking
 global load_idt
 
 ; load_idt(idt_ptr) - load idt
