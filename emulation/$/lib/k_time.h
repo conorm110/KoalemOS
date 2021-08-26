350f0f19242e4847f088b8cc04d4c7685ad2f7ff
conorm <mjoconnor110@gmail.com>
2021-08-26 18:48:04 -0500

    clear cache

--- emulation/$/lib/k_time.h
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2021, Conor Mika
- * All rights reserved.
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. 
- */
-
-extern void delay_cycle();
-void delay(int n);
