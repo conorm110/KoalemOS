7de0f616ebdd2e82bc21d273812c50f6e8b3572c
conorm <mjoconnor110@gmail.com>
2022-06-22 15:25:30 -0500

    Minor Fixes
    Moved some code from os.c to os.h to make life easier in the future

--- kernel/src/os/os.c
+++ kernel/src/os/os.c
@@ -1,21 +1,5 @@
 #include "os.h"
 
-extern void puts(char *str);
-extern char *gets();
-extern void clearScreen();
-extern void test_pci();
-extern void port_word_out(unsigned short port, unsigned short data);
-extern void init_keyboard();
-extern int strlen(const char *str);
-
-struct Point
-{
-	unsigned int X;
-	unsigned int Y;
-};
-
-extern struct Point CursorPosition;
-
 void os_main() {
     init_keyboard();
     clearScreen();
